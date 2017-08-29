f219c6c24461aaca6174747a3f395c37cf0d3dfd
Jesse Mendoza <Owner@Jesses-MacBook-Pro-2.local>
2017-08-28 22:06:32 -0700

    replaced self.exp dict with just self

--- Post-Experiment-Data-Analysis.py
+++ Post-Experiment-Data-Analysis.py
@@ -2,16 +2,12 @@
 # coding: utf-8
 
 # # Titania Coatings - Post-Experiment Data Analysis
-# This program is to take in data collected by the DAQ unit and output the results of that data, including plots and reductions
-# 
+# This program is to take in data collected by the DAQ unit and output
+# the results of that data, including plots and reductions
+#
 # Jesse Mendoza
 # edited August 2017
 
-# ## Preface
-# When calling script from bash, include filename and the times that the UV light was 
-# turned on and when reduction stopped, in the following format. Please follow it exactly, punctuation and spaces and all: 
-# 
-# \>> python PostExperimentDataAnalysis.py 'experimentdata.csv' '2:30PM-2:55PM' '3:15PM-3:43PM'
 
 '''
 TODO:
@@ -47,10 +43,9 @@ class Experiment:
 		self.header_size=8
 		# margin = number of minutes to add before and after exp times
 		self.margin = 1
-		self.exp = {}
-		self.exp['duration'] = []
-		self.exp['no_ppb'] = []
-		self.exp['nox_ppb'] = []
+		self.duration = []
+		self.no_ppb = []
+		self.nox_ppb = []
 
 
 	def get_dataset(self, file_name):
@@ -75,16 +70,16 @@ class Experiment:
 
 		margin = dt.timedelta(minutes=self.margin)
 
-		self.exp['start_time'] = formatted_start_time - margin
-		self.exp['end_time'] = formatted_end_time + margin
+		self.start_time = formatted_start_time - margin
+		self.end_time = formatted_end_time + margin
 
-		if (formatted_dt > (formatted_start_time-margin)) and (formatted_dt < (formatted_end_time+margin)):
+		if (formatted_dt>(formatted_start_time-margin)) and (formatted_dt<(formatted_end_time+margin)):
 			return True
 		else:
 			return False
 
 	def get_duration(self, formatted_dt):
-		diff = formatted_dt - self.exp['start_time']
+		diff = formatted_dt - self.start_time
 		return round(float(diff.seconds)/60,2)
 
 	def conv_no_v_to_ppb(self, no_voltage):
@@ -94,61 +89,61 @@ class Experiment:
 		return 99.626*float(nox_voltage) + 0.66
 
 	def smooth_no_ppb(self,somethin):
-		win = 60 #moving window (in seconds) 
-		self.exp['no_ppb_smooth'] = self.exp['no_ppb'].rolling(window=win,win_type='boxcar').mean()
-		self.exp['nox_ppb_smooth'] = self.exp['nox_ppb'].rolling(window=win,win_type='boxcar').mean()
-	
+		win = 60 #moving window (in seconds)
+		self.no_ppb_smooth = self.no_ppb.rolling(window=win,win_type='boxcar').mean()
+		self.nox_ppb_smooth = self.nox_ppb.rolling(window=win,win_type='boxcar').mean()
+
 	def kinetics(self):
-		'''self.exp['no_ppb_min'] = self.exp['no_ppb_smooth'].min()
-		self.exp['nox_ppb_min'] = self.exp['nox_ppb_smooth'].min()
-		self.exp['no_ppb_max'] = self.exp['no_ppb_smooth'].max()
-		self.exp['nox_ppb_max'] = self.exp['nox_ppb_smooth'].max()
-		self.exp['no_reduction'] = 1-(self.exp['no_ppb_min']/self.exp['no_ppb_max'])
-		self.exp['nox_reduction'] = 1-(self.exp['nox_ppb_min']/self.exp['nox_ppb_max'])'''
+		'''self.no_ppb_min = self.no_ppb_smooth.min()
+		self.nox_ppb_min = self.nox_ppb_smooth.min()
+		self.no_ppb_max = self.no_ppb_smooth.max()
+		self.nox_ppb_max = self.nox_ppb_smooth.max()
+		self.no_reduction = 1-(self.no_ppb_min/self.no_ppb_max)
+		self.nox_reduction = 1-(self.nox_ppb_min/self.nox_ppb_max)'''
 		pass
 
 	def run(self, datafile, exp_times):
-		self.exp['raw_data'] = self.get_dataset(datafile)
-		listed_dataset = self.remove_headers(self.exp['raw_data'])
+		self.raw_data = self.get_dataset(datafile)
+		listed_dataset = self.remove_headers(self.raw_data)
 
 		for row in listed_dataset:
 			row[1] = self.transform_to_datetime(row[1])
 			if self.filter_dataset(row[1], exp_times) == True:
-				self.exp['duration'].append(self.get_duration(row[1]))
-				self.exp['no_ppb'].append(self.conv_no_v_to_ppb(row[2]))
-				self.exp['nox_ppb'].append(self.conv_nox_v_to_ppb(row[3]))
-		
+				self.duration.append(self.get_duration(row[1]))
+				self.no_ppb.append(self.conv_no_v_to_ppb(row[2]))
+				self.nox_ppb.append(self.conv_nox_v_to_ppb(row[3]))
+
 		self.kinetics()
 
-		'''for i,row in enumerate(self.exp['duration']):
-			print row,'\t', self.exp['no_ppb'][i], '\t',self.exp['nox_ppb'][i]'''
-		return self.exp
+		'''for i,row in enumerate(self.duration):
+			print row,'\t', self.no_ppb[i], '\t',self.nox_ppb[i]'''
+		return self
 
 
 
 class Plot:
 	def __init__(self, exp):
 		self.plot_file_path = ''
-	
+
 		'''start_dt = str(exp.dataset['Date&Time'].iloc[0][0:16])+''+str(exp.dataset['Date&Time'].iloc[0][23:])
 		plt.title('NOx Reduction\n'+'Experiment Starting @ '+ start_dt)'''
-		
-		no=plt.plot(exp['duration'], exp['no_ppb'],label='NO')
-		nox=plt.plot(exp['duration'], exp['nox_ppb'], label='NOx', color='r', linestyle='--')
-		
+
+		no=plt.plot(exp.duration, exp.no_ppb,label='NO')
+		nox=plt.plot(exp.duration, exp.nox_ppb, label='NOx', color='r', linestyle='--')
+
 		plt.xlabel('Duration (min)',size=12)
 		plt.ylabel('Concentration (ppb)',fontsize=12)
-		
+
 		plt.legend(loc='best')
 
 		'''plt.figtext(0.765,0.72,'Reductions', weight='bold')
 		plt.figtext(0.765,0.68,'NO: '+str(round(exp.no_reduction*100,1))+'%')
 		plt.figtext(0.765,0.64,'NOx: '+str(round(exp.nox_reduction*100,1))+'%')'''
-		
+
 		#save_filename = str(start_dt[0:10]+'_'+exp.name).replace('/','-')
 		save_filename = "Titania DAQ test"
 		pylab.savefig('/Users/Owner/Desktop/'+save_filename)
-		
+
 		plt.show()
 
 
@@ -172,7 +167,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
-
-
